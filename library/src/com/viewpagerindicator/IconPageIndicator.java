12f66ea335dd6bd4954cd293258808fbd536d60f
Tsunehito Takara <tsunehito.takara@l-is-b.com>
2013-05-01 22:06:46 +0900

    Add new tap actions.
    Add these actions.
    
    - If indicator is tapped, jump to this page.
    - If left padding is tapped, move to left page.
    - If right padding is tapped, move to right page.
    
    Alternatively, remove horizontal scroll function if width overflow.

--- library/src/com/viewpagerindicator/IconPageIndicator.java
+++ library/src/com/viewpagerindicator/IconPageIndicator.java
@@ -16,30 +16,40 @@
  */
 package com.viewpagerindicator;
 
+import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewConfigurationCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.HorizontalScrollView;
+import android.view.ViewConfiguration;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-
-import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
  * This widget implements the dynamic action bar tab behavior that can change
  * across different configurations or circumstances.
  */
-public class IconPageIndicator extends HorizontalScrollView implements PageIndicator {
+public class IconPageIndicator extends FrameLayout implements PageIndicator {
+    private static final int INVALID_POINTER = -1;
+
     private final IcsLinearLayout mIconsLayout;
 
     private ViewPager mViewPager;
     private OnPageChangeListener mListener;
     private Runnable mIconSelector;
     private int mSelectedIndex;
+
+    private int mTouchSlop;
+    private float mLastMotionX = -1;
+    private int mActivePointerId = INVALID_POINTER;
+    private boolean mIsDragging;
 
     public IconPageIndicator(Context context) {
         this(context, null);
@@ -51,21 +61,9 @@ public class IconPageIndicator extends HorizontalScrollView implements PageIndic
 
         mIconsLayout = new IcsLinearLayout(context, R.attr.vpiIconPageIndicatorStyle);
         addView(mIconsLayout, new LayoutParams(WRAP_CONTENT, FILL_PARENT, Gravity.CENTER));
-    }
 
-    private void animateToIcon(final int position) {
-        final View iconView = mIconsLayout.getChildAt(position);
-        if (mIconSelector != null) {
-            removeCallbacks(mIconSelector);
-        }
-        mIconSelector = new Runnable() {
-            public void run() {
-                final int scrollPos = iconView.getLeft() - (getWidth() - iconView.getWidth()) / 2;
-                smoothScrollTo(scrollPos, 0);
-                mIconSelector = null;
-            }
-        };
-        post(mIconSelector);
+        final ViewConfiguration configuration = ViewConfiguration.get(context);
+        mTouchSlop = ViewConfigurationCompat.getScaledPagingTouchSlop(configuration);
     }
 
     @Override
@@ -159,14 +157,104 @@ public class IconPageIndicator extends HorizontalScrollView implements PageIndic
             View child = mIconsLayout.getChildAt(i);
             boolean isSelected = (i == item);
             child.setSelected(isSelected);
-            if (isSelected) {
-                animateToIcon(item);
-            }
         }
     }
 
     @Override
     public void setOnPageChangeListener(OnPageChangeListener listener) {
         mListener = listener;
+    }
+
+    @Override
+    public boolean onTouchEvent(android.view.MotionEvent ev) {
+        if (super.onTouchEvent(ev)) {
+            return true;
+        }
+        if ((mViewPager == null) || (mViewPager.getAdapter().getCount() == 0)) {
+            return false;
+        }
+
+        final int action = ev.getAction() & MotionEventCompat.ACTION_MASK;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mActivePointerId = MotionEventCompat.getPointerId(ev, 0);
+                mLastMotionX = ev.getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE: {
+                final int activePointerIndex = MotionEventCompat.findPointerIndex(ev, mActivePointerId);
+                final float x = MotionEventCompat.getX(ev, activePointerIndex);
+                final float deltaX = x - mLastMotionX;
+
+                if (!mIsDragging) {
+                    if (Math.abs(deltaX) > mTouchSlop) {
+                        mIsDragging = true;
+                    }
+                }
+
+                if (mIsDragging) {
+                    mLastMotionX = x;
+                    if (mViewPager.isFakeDragging() || mViewPager.beginFakeDrag()) {
+                        mViewPager.fakeDragBy(deltaX);
+                    }
+                }
+
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                if (!mIsDragging) {
+                    final int count = mViewPager.getAdapter().getCount();
+
+                    if ((mSelectedIndex > 0) && (ev.getX() < mIconsLayout.getLeft())) {
+                        if (action != MotionEvent.ACTION_CANCEL) {
+                            mViewPager.setCurrentItem(mSelectedIndex - 1);
+                        }
+                        return true;
+                    } else if ((mSelectedIndex < count - 1) && (ev.getX() > mIconsLayout.getRight())) {
+                        if (action != MotionEvent.ACTION_CANCEL) {
+                            mViewPager.setCurrentItem(mSelectedIndex + 1);
+                        }
+                        return true;
+                    } else {
+                        // If indicator is tapped, jump to it's page.
+                        if (action != MotionEvent.ACTION_CANCEL) {
+                            final float x = ev.getX() - mIconsLayout.getLeft();
+                            for (int i = 0, size = mIconsLayout.getChildCount(); i < size; i++) {
+                                final View child = mIconsLayout.getChildAt(i);
+                                if (x >= child.getLeft() && x < child.getRight()) {
+                                    mViewPager.setCurrentItem(i);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                mIsDragging = false;
+                mActivePointerId = INVALID_POINTER;
+                if (mViewPager.isFakeDragging()) mViewPager.endFakeDrag();
+                break;
+
+            case MotionEventCompat.ACTION_POINTER_DOWN: {
+                final int index = MotionEventCompat.getActionIndex(ev);
+                mLastMotionX = MotionEventCompat.getX(ev, index);
+                mActivePointerId = MotionEventCompat.getPointerId(ev, index);
+                break;
+            }
+
+            case MotionEventCompat.ACTION_POINTER_UP:
+                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mActivePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
+                }
+                mLastMotionX = MotionEventCompat.getX(ev, MotionEventCompat.findPointerIndex(ev, mActivePointerId));
+                break;
+        }
+
+        return true;
     }
 }
